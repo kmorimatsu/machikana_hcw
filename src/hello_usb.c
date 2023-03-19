90534f868bcce373e84cf69ca2608aba5262ffa2
Katsumi <51002934+kmorimatsu@users.noreply.github.com>
2023-03-18 22:12:48 -0700

    int add_values(int a, int b);

--- src/hello_usb.c
+++ src/hello_usb.c
@@ -7,10 +7,17 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+int add_values(int a, int b){
+	return a+b;
+}
+
 int main() {
+	int a=0,b=0;
 	stdio_init_all();
 	while (true) {
-		printf("Hello, world!\n");
+		printf("%d + %d = %d\n", a, b, add_values(a,b));
+		a+=1;
+		b+=2;
 		sleep_ms(1000);
 	}
 	return 0;
